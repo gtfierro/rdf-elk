eecd2ae691f40524c5ce323e30a4f35009c3f775
Gabe Fierro <gtfierro@mines.edu>
2022-05-17 15:40:35 -0600

    add sensors!

--- make223p.py
+++ make223p.py
@@ -68,15 +68,9 @@ for row in res:
         "width": 8,
         "height": 8,
         "labels": [{"text": str(cplabel) if cplabel else nid(connpoint)}],
-        "class": ["port", "inlet"],
+        "class": ["port", "connectionpoint"],
         #TODO: use direction/medium to place on side of node?
     }
-
-for node_defn in nodes.values():
-    my_ports = node_defn.pop("ports")
-    node_defn["ports"] = [
-        ports[port_id] for port_id in my_ports
-    ]
 
 
 res = g.query("""SELECT DISTINCT ?n1 ?n2 ?cp1 ?cp2 WHERE {
@@ -108,7 +102,7 @@ for row in res:
         "attributes": {"marker-end": "url(#arrow)"}
     })
 
-# get properties
+# get properties on connection points
 res = g.query("""SELECT DISTINCT ?node ?loc ?prop ?proplabel WHERE {
         ?loc s223:hasProperty ?prop .
         ?loc s223:isConnectionPointOf ?node .
@@ -148,6 +142,92 @@ for row in res:
         #"attributes": {"marker-end": "url(#arrow)"}
     })
 
+# get properties on devices
+res = g.query("""SELECT DISTINCT ?node ?prop ?proplabel WHERE {
+        ?node s223:hasProperty ?prop .
+        ?node rdf:type/rdfs:subClassOf* s223:Device .
+        ?prop rdf:type/rdfs:subClassOf* s223:Property .
+        OPTIONAL {?prop rdfs:label ?proplabel}
+}""")
+seen_props = set()
+for row in res:
+    (node, prop, plabel) = row
+    node = nid(node)
+    prop = nid(prop)
+
+    # only add each property once; otherwise we can get multiple edges and it is confusing
+    if prop in seen_props:
+        continue
+    seen_props.add(prop)
+
+    # add 'property' as a new node
+    nodes[prop]["id"] = prop
+    nodes[prop]["width"] = 120
+    nodes[prop]["height"] = 20
+    nodes[prop]["labels"] = [{"text": str(plabel) if plabel else prop, "height": 20, "width": 20}]
+    nodes[prop]["nodeSize.constraints"] =  "[PORTS, MINIMUM_SIZE]"
+    nodes[prop]["layoutOptions"] =  {
+        "nodeLabels.placement": "[INSIDE]",
+    }
+    nodes[prop]["ports"] = []
+    nodes[prop]["class"] = ["node", "s223property"]
+
+    # add edge to property
+    doc["edges"].append({
+        "id": gensym(),
+        "sources": [node],
+        "targets": [prop],
+        "attributes": {"stroke-dasharray": "2,2"},
+    })
+
+# get sensors for properties
+res = g.query("""SELECT DISTINCT ?sensor ?prop ?senslabel WHERE {
+        ?sensor s223:observesProperty ?prop .
+        ?prop rdf:type/rdfs:subClassOf* s223:Property .
+        OPTIONAL {?sensor rdfs:label ?senslabel}
+}""")
+for row in res:
+    (sensor, prop, slabel) = row
+    sensor = nid(sensor)
+    prop = nid(prop)
+
+    # add sensor node
+    nodes[sensor]["id"] = sensor
+    nodes[sensor]["width"] = 160
+    nodes[sensor]["height"] = 20
+    nodes[sensor]["labels"] = [{"text": str(slabel) if slabel else sensor, "height": 20, "width": 20}]
+    nodes[sensor]["nodeSize.constraints"] =  "[PORTS, MINIMUM_SIZE]"
+    nodes[sensor]["layoutOptions"] =  {
+        "nodeLabels.placement": "[INSIDE]",
+    }
+    nodes[sensor]["ports"] = [f"{sensor}:observes"]
+    nodes[sensor]["class"] = ["node", "sensor"]
+
+    ports[f"{sensor}:observes"] = {
+        "id": f"{sensor}:observes",
+        "layoutOptions": {
+            "portLabels.placement": "[INSIDE]",
+        },
+        "labels": [{"text": "observes"}],
+        "class": ["port"],
+    }
+
+    # add edge to property
+    doc["edges"].append({
+        "id": gensym(),
+        "sources": [f"{sensor}:observes"],
+        "targets": [prop],
+        #"attributes": {"stroke-dasharray": "2,2"},
+        #"attributes": {"marker-end": "url(#arrow)"},
+        #"labels": [{"text": "observes"}],
+    })
+
+
+for node_defn in nodes.values():
+    my_ports = node_defn.pop("ports")
+    node_defn["ports"] = [
+        ports[port_id] for port_id in my_ports
+    ]
 
 for node in nodes.values():
     doc["children"].append(node)
@@ -160,6 +240,10 @@ rect {
   stroke: #222222;
 }
 rect.port {
+  opacity: 1;
+  fill: #6094CC;
+}
+rect.connectionpoint {
   opacity: 1;
   fill: #FD4659;
 }
@@ -185,6 +269,9 @@ path {
 }
 rect.s223property {
   fill: #32BF84;
+}
+rect.sensor {
+  fill: #9DBCD4;
 }
 """
 
